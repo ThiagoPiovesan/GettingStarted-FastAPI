fd85ae54f96d0f5abeb02aae45e3bc27d0c709e2
Thiago Piovesan <thiagoppiovesan@gmail.com>
2022-11-23 21:10:04 -0300

    request body class

--- main.py
+++ main.py
@@ -12,6 +12,12 @@
 # Bibliotecas utilizadas:
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    id: int
+    descricao: str
+    valor: float
 
 # Criação do app
 app = FastAPI()
@@ -25,3 +31,8 @@ def read_root():
 @app.get("/items/{item_id}")                        # Método GET
 def read_item(item_id: int, p: bool, q: Union[str, None] = None):
     return {"item_id": item_id, "p": p, "q": q}
+
+@app.post("/item")
+def add_item(novo_item: Item):
+    return novo_item
+    
