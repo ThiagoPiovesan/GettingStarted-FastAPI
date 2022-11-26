feabdda84afc703a197be79989dc0eba89310fdb
Thiago Piovesan <thiagoppiovesan@gmail.com>
2022-11-26 14:40:16 -0300

    Class004 - Creating Firt simple API

--- main.py
+++ main.py
@@ -11,42 +11,34 @@
 #==================================================================================================#
 # Bibliotecas utilizadas:
 from typing import Union
-from fastapi import FastAPI, Header, Response, Cookie
+from fastapi import FastAPI
 from pydantic import BaseModel
 
 class Item(BaseModel):
     id: int
+    quantidade: int
     descricao: str
     valor: float
 
 # Criação do app
 app = FastAPI()
 
-# definição das rotas
-@app.get("/")                                       # Método GET
-def read_root(user_agent: Union[str, None] = Header(None)):
-    return {"user_agent": user_agent}
+# fake database
+banco_de_dados = []
 
-# definição das rotas
-@app.get("/cookie")                                 # Método GET
-def cookie(response: Response):
-# Informações gravadas do lado do cliente, gravar alguma sessão
-# ou preferência do usuário.
-    response.set_cookie(key="meucookie", value="123-456")
-    return {"cookie": True}
+# Endpoint to create the item
+@app.post("/item")
+def add_item(item: Item):
+    banco_de_dados.append(item)
+    return item
 
-# definição das rotas
-@app.get("/get-cookie")                             # Método GET
-def get_cookie(meucookie: Union[str, None] = Cookie(None)):
-    return {"Cookie": meucookie}
+# Endpoint to get all the objects added on database
+@app.get("/item")
+def list_item():
+    return banco_de_dados
 
-# definição das rotas
-@app.get("/items/{item_id}")                        # Método GET
-def read_item(item_id: int, p: bool, q: Union[str, None] = None):
-    return {"item_id": item_id, "p": p, "q": q}
-
-# definição das rotas
-@app.post("/item")                                  # Método POST       
-def add_item(novo_item: Item):
-    return novo_item
+@app.get("/item/valor_total")
+def get_valor_total():
+    valor_total:float = sum(item.valor * item.quantidade for item in banco_de_dados)
     
+    return {"Valor_total: ": valor_total}
